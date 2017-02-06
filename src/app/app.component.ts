5c14ccad7b837b1b6e0027a356ff8bd35451e1ca
Sefi Ninio <sninio@vmware.com>
2017-02-06 13:33:34 +0200

    change `useStrict` usage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from "@angular/core";
 import { Router } from "@angular/router";
 import { useStrict } from "mobx";
 
+useStrict(true);
+
 @Component({
     selector: 'my-app',
     templateUrl: './app.component.html',
@@ -10,7 +12,6 @@ import { useStrict } from "mobx";
 export class AppComponent {
 
     constructor(private router: Router) {
-        useStrict(true);
     }
 
 }
